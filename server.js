fe23709a7e0134ea2b3e31e8322cea2d174b9f25
mohdmustfa <md.mustfamalik786@gmail.com>
2026-05-13 19:22:44 +0530

    update backend

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ import {
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
     methods: ["GET", "POST"],
-  }));
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-
   res.json({
     success: true,
     message: "Accessibility Auditor API running",
   });
-
 });
 
 app.post("/scan", async (req, res) => {
@@ -28,24 +29,25 @@ app.post("/scan", async (req, res) => {
 
     const { url } = req.body;
 
-    const results = await runAccessibilityScan(url);
+    const results =
+      await runAccessibilityScan(url);
 
     res.json(results);
 
   } catch (error) {
 
-    console.log("SCAN ERROR:");
-  console.log(error);
+    console.log(error);
 
     return res.status(500).json({
-        success: false,
-        message: "Sorry, could not fetch data for this URL. Check your URL and try again.", 
-        // message: error.message,
+      success: false,
+      message: error.message,
     });
+
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
